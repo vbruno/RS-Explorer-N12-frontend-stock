b948ba921562c969710ab6e305a50a0fb8770c2a
Bruno S Velho <bruno.velho@gmail.com>
2023-11-06 08:33:49 -0300

    Separando as Rotas

--- src/pages/Product/styles.js
+++ src/pages/Product/styles.js
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   width: 100%;
@@ -27,7 +27,6 @@ export const Header = styled.header`
     }
   }
 `;
-
 
 export const Item = styled(Link)`
    width: 100%;
